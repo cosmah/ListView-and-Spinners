00262c41a4cd800510e2ed65c4ffb116af72b901
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-09-13 13:06:43 +0300

    spinners

--- app/src/main/java/com/cosmah/listview_and_spinners/MainActivity.java
+++ app/src/main/java/com/cosmah/listview_and_spinners/MainActivity.java
@@ -6,23 +6,73 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    //declare listview
+    //declare listview & spinner
     private ListView citiesList;
+    private Spinner studentsSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //fb(create list)
+        //fb(create list) initialise
         citiesList = findViewById(R.id.cities);
+        studentsSpinner = findViewById(R.id.studentsSpinner);
 
+        ArrayList<String> students = new ArrayList<>();
+        students.add("John");
+        students.add("Heidi");
+        students.add("Marvin");
+        students.add("Harry");
+        students.add("Estella");
+        students.add("Phionah");
+        students.add("Tom");
+
+//        //create adaptor to fetch data
+//        ArrayAdapter<String> studentsAdapter = new ArrayAdapter<>(
+//                //context
+//                this,
+//                //layout
+//                android.R.layout.simple_spinner_dropdown_item,
+//                //values
+//                students
+//        );
+//
+//        studentsSpinner.setAdapter(studentsAdapter);
+//
+//        studentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(MainActivity.this, students.get(position) + " is Selected.", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+
+        studentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, studentsSpinner.getSelectedItem().toString() + " is Selected.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+//list view
         ArrayList<String> cities = new ArrayList<>();
         cities.add("Kampala");
         cities.add("Nairobi");
